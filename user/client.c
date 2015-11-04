5b405c527891982c3fb336ef7dbcee93e400c3b2
Stephan <CaptainRoot@users.noreply.github.com>
2015-11-04 07:45:38 +0100

    Update client.c
    fix error

--- user/client.c
+++ user/client.c
@@ -46,7 +46,6 @@ void ICACHE_FLASH_ATTR networkReconCb(void *arg, sint8 err)
 
 void ICACHE_FLASH_ATTR networkDisconCb(void *arg) 
 {
-{
   //uart0_tx_buffer("dcon",4);
   // Thank to Familienpapi@FHEM-Foum
   os_timer_disarm(&network_timer);
